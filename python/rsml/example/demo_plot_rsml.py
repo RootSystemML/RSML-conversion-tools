a4c9e4910ab14371baf3f1cffe555726a2fce1fc
julien-diener <julien.diener@inria.fr>
2014-10-17 16:11:20 +0200

    updated example/demo_plot_rsml.py

--- python/rsml/example/demo_plot_rsml.py
+++ python/rsml/example/demo_plot_rsml.py
@@ -11,37 +11,56 @@ import os
 
 # load rsml
 rsml_dir = shared_data(rsml.__path__)#,'AR570/2012_11_25_09h00_chl110_1_1.rsml')
-rsml_files = glob(rsml_dir/"AR570/*.rsml")
+rsml_files = sorted(glob(rsml_dir/"AR570/*.rsml"))
 
-plt.ion()
-plt.clf()
-ax = None
-for i,rsml_file in enumerate(sorted(rsml_files)):
-    filename = os.path.split(rsml_file)[-1]
-    print filename
-    g = rsml.rsml2mtg(rsml_file)
+def plot(x,y, label):
+    # fit and plot linear regression
+    a,b = np.polyfit(x,y,1)
+    X = np.array([0,x.max()])
+    l = plt.plot(x,y, '.')[0]
+    plt.plot(X,a*X+b,l.get_color(), label=label+"(a=%.2f)"%a)
+    ax.legend(loc=0)
     
+def plot_from_file(rsml_file, ax, split_plant, label=""):
+    g = rsml.rsml2mtg(rsml_file)
+
     # extract properties & measurments for all roots
-    root = measurements.root_order(g)               # ids of lalteral roots
+    root = measurements.root_order(g)               # ids of lateral roots
     root = [r for r,o in root.iteritems() if o==2]
     length = measurements.root_length(g,root)       # length of roots
     ppos = measurements.parent_position(g,root)     # branching position on parent
     plant = dict((r,g.complex(r)) for r in root)    # plant id of all roots
     
-    ax = plt.subplot(len(rsml_files),1,i+1, sharex=ax, sharey=ax)
+    if split_plant:
+        # plot root length w.r.t parent_position, for each plant
+        for i,pid in enumerate(sorted(set(plant.values()))):
+            rids = [r for r,p in plant.iteritems() if p==pid]
+            x = np.array([ppos[r]   for r in rids])
+            y = np.array([length[r] for r in rids])
+            plot(x,y,"plant %d"%(i+1))
+
+    else:
+        x = np.array([ppos[r]   for r in root])
+        y = np.array([length[r] for r in root])
+        plot(x,y, label)
+
+
+plt.ion()
+plt.clf()
+
+
+# plot last time step, for each plant
+ax = plt.subplot(2,1,1)
+filename = os.path.split(rsml_files[-1])[-1]
+plot_from_file(rsml_files[-1], ax, split_plant=True)
+ax.set_title("Individual plants at day 12")    
     
-    # plot root length w.r.t parent_position, for each plant
-    for pid in sorted(set(plant.values())):
-        rids = [r for r,p in plant.iteritems() if p==pid]
-        x = np.array([ppos[r]   for r in rids])
-        y = np.array([length[r] for r in rids])
-        o = x.argsort()
-        l = plt.plot(x[o],y[o], '.')[0]
-        
-        # fit and plot linear regression
-        a,b = np.polyfit(x,y,1)
-        X = np.array([0,x.max()])
-        plt.plot(X,a*X+b,l.get_color(), label="a=%.2f"%a)
+# plot for each time step, no plant distinction 
+ax = plt.subplot(2,1,2, sharex=ax, sharey=ax)
+days = [6,7,8,10,11,12]
+for i,rsml_file in enumerate(rsml_files):
+    plot_from_file(rsml_file, ax, split_plant=False, label="day %2d"%days[i])
+ax.set_title("All plants from day 6 to 12")    
     
-    ax.set_title(filename)    
-    ax.legend(loc=0)
+    
+
